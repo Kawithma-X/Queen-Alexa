70c4cc2465c6ab165f1ce5bfaa43251474c579aa
ChamodKeshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-04-20 07:31:21 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,1 +1,22 @@
+/* Code by (C) 2022 ChamodKeshan - Queen Alexa.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+ChamodKeshan - Alexa Team
+*/
 
+function successfullMessage(msg) {
+    return "👩‍🦰 *QUEEN ALEXA*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🚀 *QUEEN ALEXA*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "🤖 *QUEEN ALEXA*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
